6db9e24f061eded743dbc95a27cf93a6a0eabde2
sorima234 <103243758+sorima234@users.noreply.github.com>
2022-06-21 17:01:37 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+var header = document.getElementById("menu");
+var menuItems = header.getElementsByTagName("li");
 function messageSent(){
     document.getElementById("message-sent").innerHTML = "Your message has been sent. Thank you!";
 }
 function scrollToTop(){
     window.scrollTo(0, 0);
+    var current1 = document.getElementsByClassName("current-selected");
+    current1[0].className = current1[0].className.replace("current-selected", "");
+    menuItems[0].className += "current-selected";
 }
-var header = document.getElementById("menu");
-var menuItems = header.getElementsByTagName("li");
+function goToAbout(){
+    var current2 = document.getElementsByClassName("current-selected");
+    current2[0].className = current2[0].className.replace("current-selected", "");
+    menuItems[1].className += "current-selected";
+}
 for (var i = 0; i < menuItems.length; i++){
     menuItems[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("current-selected");
@@ -13,5 +21,3 @@ for (var i = 0; i < menuItems.length; i++){
     this.className += "current-selected";
     });
 }
-console.log(header);
-console.log(menuItems);
